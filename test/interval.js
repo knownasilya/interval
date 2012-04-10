d0fd51a3e3124d39de9b592e8beb28c7aa8e928e
J. Jordan <jordan@styosis.com>
2012-04-10 09:12:43 -0400

    add creational method to convert from milliseconds

--- test/interval.js
+++ test/interval.js
@@ -2,7 +2,18 @@ var expect = require('expect.js');
 var interval = require('../');
 
 describe('interval', function () {
- 
+
+  // zero undefined properties
+  function create(i) {
+    var props = ['days', 'hours', 'minutes', 'seconds', 'milliseconds'];
+    props.forEach(function (prop) {
+      if (!i[prop]) {
+        i[prop] = 0;
+      }
+    });
+    return i;
+  }
+
   it('should convert days property to milliseconds', function () {
     expect(interval({ days: 4 })).to.be(4*24*60*60*1000);
   });
@@ -29,6 +40,18 @@ describe('interval', function () {
   
   it('should stringify minutes', function () {
     expect(interval.stringify({ minutes: 1 })).to.be('1 minutes');
+  });
+  
+  it('should build object from milliseconds', function () {
+    var actual = interval.fromMilliseconds(50 * 1000 + 40);
+    var expected = create({ seconds: 50, milliseconds: 40 });
+    expect(actual).to.eql(expected);
+  });
+
+  it('should normalize over accounted places', function () {
+    var source = { seconds: 75 };
+    var expected = create({ minutes: 1, seconds: 15 });
+    expect(interval.normalize(source)).to.eql(expected);
   });
 
 });
